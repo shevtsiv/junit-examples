16dd04cd57c871e230bb24ac6a3cff64f8c7b181
shevtsiv <rostykshevtsiv@gmail.com>
2020-08-02 15:59:16 +0300

    Implement Argument Providers examples

---
+++ src/main/java/me/shevtsiv/Calculator.java
@@ -0,0 +1,7 @@
+package me.shevtsiv;
+
+public class Calculator {
+    public int sumTwoNumbers(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/me/shevtsiv/parametrized/ArgumentProviderTest.java
@@ -0,0 +1,66 @@
+package me.shevtsiv.parametrized;
+
+import me.shevtsiv.Calculator;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ArgumentProviderTest {
+
+    private static List<Integer> provideListOfSingleIntegers() {
+        return List.of(1, 3, 5, 7, 9);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideListOfSingleIntegers")
+    public void provideListOfSingleIntegersTest(Integer intParameter) {
+        assertNotEquals(0, intParameter % 2);
+    }
+
+    @ParameterizedTest
+    @MethodSource // Note: Test method name is the same as provider method name, so no explicit declaration
+    public void provideListOfSingleIntegers(Integer intParameter) {
+        assertNotEquals(0, intParameter % 2);
+    }
+
+    @ParameterizedTest
+    @MethodSource("me.shevtsiv.parametrized.AnotherClassWithMethodProvider#provideListOfSingleIntegersFromAnotherClass")
+    public void provideListOfSingleIntegersFromAnotherClass(Integer intParameter) {
+        assertEquals(0, intParameter % 2);
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(RandomPairOfIntegersArgumentProvider.class)
+    public void customInfiniteRandomPairOfIntegersArgumentProvider(int a, int b) {
+        Calculator calculator = new Calculator();
+        assertEquals(a + b, calculator.sumTwoNumbers(a, b));
+    }
+}
+
+@SuppressWarnings("unused") // Used by ArgumentProviderTest#provideListOfSingleIntegersFromAnotherClass
+class AnotherClassWithMethodProvider {
+    private static List<Integer> provideListOfSingleIntegersFromAnotherClass() {
+        return List.of(0, 2, 4, 6, 8);
+    }
+}
+
+class RandomPairOfIntegersArgumentProvider implements ArgumentsProvider {
+    @Override
+    public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+        Random random = new Random();
+        return Stream.of(
+                Arguments.of(random.nextInt(), random.nextInt()),
+                Arguments.of(random.nextInt(), random.nextInt()),
+                Arguments.of(random.nextInt(), random.nextInt())
+        );
+    }
+}
